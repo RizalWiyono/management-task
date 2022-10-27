294ca5fb338ea54ae3d852bef0ec646586013485
Muhammad Rizal Wiyono <69446968+RizalWiyono@users.noreply.github.com>
2022-10-27 21:42:09 +0700

    chore: style add task

--- component/scrum-master/task/index.php
+++ component/scrum-master/task/index.php
@@ -87,6 +87,15 @@
             <h1>
             <?=$row['title']?>
             </h1>
+            <?php
+            if($row['point'] !== 0){
+              echo "<h3 style='float: left; margin: 0;'>".$row['point']."</h3><h2 style='float: left; margin: 0; margin-top: 12px; margin-left: 5px;'>Point</h2>";
+            }elseif($row['priority'] === 'Medium'){
+              echo "<h2>".$row['point']." Point</h2>";
+            }else{
+              echo "<span style='color: #F33838;'>Hard</span>";
+            }
+            ?>
           </div>
           <div class="d-flex right-side-car">
             <div class="component">
@@ -109,7 +118,13 @@
               }
               ?>
             </div>
-            <button class="ml-4" style="background: #FF7F3F; border-radius: 10px; z-index: 2;">></button>
+            <?php
+            if($row['status'] === 'Publish'){
+              echo '<button class="ml-4" style="background: #FF7F3F; border: 0; border-radius: 10px; z-index: 2;">Progress</button>';
+            }elseif($row['status'] === 'Verification'){
+              echo '<button class="ml-4" style="background: #38F378; border: 0; border-radius: 10px; z-index: 2;">Done</button>';
+            }
+            ?>
           </div>
         </div>
       <?php $no++;
