326103cbac5bfb89a15983596b2e22ff3915e103
Muhammad Rizal Wiyono <69446968+RizalWiyono@users.noreply.github.com>
2022-10-27 23:28:25 +0700

    chore: user notes feature

--- component/user/note/index.php
+++ component/user/note/index.php
@@ -1,13 +1,12 @@
 <?php
     include '../../../src/connection/connection.php';
-    $idboards = $_GET['id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>User || Task</title>
+	<title>User || Notes</title>
 
 	<!-- Favicon -->
 	<link rel="icon" type="image/x-icon" href="../../../src/images/favicon.png">
@@ -45,6 +44,7 @@
 <div class="container-fluid">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
+      
       <div class="sidebar-sticky">
         <h1 class="title-logo mb-4">TaskManager.</h1>
         <ul class="nav flex-column">
@@ -72,72 +72,60 @@
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4" style="height: 100vh;">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Task Boards</h1>
+        <h1 class="h2">Notes</h1>
       </div>
       <?php
         $no = 1;
-        $queryTask  = mysqli_query($connect, "SELECT * FROM tb_task WHERE idboards=$idboards");
-        while($row = mysqli_fetch_array($queryTask)){?>
-        <div class="card-task p-4">
-          <div class="component-left-decoration">
-            
-          </div>
-          <div class="component-right-decoration">
-            
-          </div>
-          <div class="left-side-car">
-            <h1>
-            <?=$row['title']?>
-            </h1>
-            <div class="d-flex align-items-center">
-              <?php
-              if($row['point'] !== 0){
-                echo "<h3 style='float: left; margin: 0;'>".$row['point']."</h3><h2 style='float: left; margin: 0; margin-top: 12px; margin-left: 5px;'>Point</h2>";
-              }elseif($row['salary'] !== 'Medium'){
-                echo "<h3 style='float: left; margin: 0;'>".$row['salary']."</h3><h2 style='float: left; margin: 0; margin-top: 12px; margin-left: 5px;'>.00</h2>";
-              }else{}
-              ?>
-            </div>
-          </div>
-          <div class="d-flex right-side-car align-items-center">
-            <div class="component">
-              <h2>Startdate</h2>
-              <span style="color: #548CFF;"><?=$row['startdate']?></span>
-            </div>
-            <div class="ml-4 component">
-              <h2>Deadline</h2>
-              <span style="color: #F33838 !important;"><?=$row['deadline']?></span>
-            </div>
-            <div class="ml-4 component">
-              <h2>Priority</h2>
-              <?php
-              if($row['priority'] === 'Low'){
-                echo "<span style='color: #38F378;'>Low</span>";
-              }elseif($row['priority'] === 'Medium'){
-                echo "<span style='color: #E3C629;'>Medium</span>";
-              }else{
-                echo "<span style='color: #F33838;'>Hard</span>";
-              }
-              ?>
-            </div>
-            <?php
-            if($row['status'] === 'Publish'){
-              echo '<form action="fetch/validationTask.php" method="post" style="z-index: 2;">
-                <input type="hidden" value="'.$idboards.'" name="paramId">
-                <input type="hidden" value="'.$row['idtask'].'" name="param">
-                <button type="submit" class="ml-4" style="background: #FF7F3F; border: 0; border-radius: 10px; z-index: 2; height: 100%;">></button>
-              </form>';
-            }elseif($row['status'] === 'Verification'){
-              echo '<button class="ml-4" style="background: #E3C629; border: 0; border-radius: 10px; z-index: 2; height: 100%;">Validation</button>';
-            }elseif($row['status'] === 'DOnes'){
-              echo '<button class="ml-4" style="background: #38F378; border: 0; border-radius: 10px; z-index: 2; height: 100%;">Done</button>';
-            }
-            ?>
-          </div>
+        $queryNote  = mysqli_query($connect, "SELECT * FROM tb_note");?>
+          <div class="row">
+            <?php while($row = mysqli_fetch_array($queryNote)){?>
+              <div class="col-md-4 mb-4">
+                <div class="card-notes col-md-12 overflow-hidden p-3">
+                  <div class="component-right-decoration"></div>
+                  <h1><?=$row['title']?></h1>
+                  <p><?=$row['description']?></p>
+                  <p style="margin: 0;
+                  float: right;
+                  z-index: 2;"><?=$row['date']?></p>
+                </div>
+              </div>
+            <?php $no++;
+            } ?> 
         </div>
-      <?php $no++;
-      } ?> 
     </main>
+
+    <button type="button" data-toggle="modal" data-target="#exampleModal" class="btn-linear-primary p-2 btn-fixed-bottom" >+ New Note</button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">New Notes</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form action="fetch/inputData.php" method="POST">
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="exampleInputEmail1">Title</label>
+              <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Title here...">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Description</label>
+              <textarea name="desc" id="" class="form-control" rows="3">Description here...
+              </textarea>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </body>
